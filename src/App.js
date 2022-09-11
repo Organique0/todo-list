90ef2c9f8379e3c51d070eb74ac94b2b868e47c4
Organique0 <grabnar.luka@gmail.com>
2022-09-11 22:44:10 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Todo from "./componets/Todo";
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React cours" />
+    </div>
+  );
 }
 
 export default App;
